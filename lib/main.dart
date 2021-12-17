029113d3823356d3d9c146bbceac40aff7504193
Eugene Beetle <eugene.beetle.01@gmail.com>
2021-12-17 10:11:40 +0200

    feat: add change color onTap event

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  Color getRandomColor() => Color(Random().nextInt(0xffffffff)).withAlpha(0xff);
+  Color? _randomScaffoldBackgroundColor;
+
+  void refreshColor() {
+    setState(() {
+      _randomScaffoldBackgroundColor = getRandomColor();
+    });
+  }
+
+  @override
+  void initState() {
+    refreshColor();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'RandomColor Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor:
+            _randomScaffoldBackgroundColor ?? Colors.blueAccent,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hey there'),
+      home: GestureDetector(
+        onTap: refreshColor,
+        // // if we want change color on any release
+        // onPanEnd: (_) => refreshColors(),
+        child: const Scaffold(
+          body: Center(
+            child: Text(
+              'Hey there',
+              style: TextStyle(
+                fontSize: 28,
+              ),
+            ),
+          ),
         ),
       ),
     );
